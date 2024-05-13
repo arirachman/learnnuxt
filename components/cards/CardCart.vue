c4677a1eef66ea7a2f3a01fcff574364ae394827
ARI RACHMAN <ari.rachmanhakiem@gmail.com>
2024-05-13 17:46:21 +0700

    product in cart can be added

--- components/cards/CardCart.vue
+++ components/cards/CardCart.vue
@@ -2,6 +2,16 @@
 const props = defineProps({
   product: { type: Object, default: {} },
 });
+
+const quantity = ref(1);
+const increaseQuantity = () =>{
+    quantity.value++;
+}
+const decreaseQuantity = () =>{
+    if (quantity.value >1){
+        quantity.value--;
+    }
+}
 </script>
 
 <template>
@@ -14,9 +24,9 @@ const props = defineProps({
       <p class="text-lg font-light mb-3">${{ props.product.price }}</p>
       <div class="flex items-center justify-between">
         <div class="flex items-center border border-gray-300 w-max rounded- full">
-          <button class="cursor-pointer py-1 pl-4 pr-2 text-lg flex items- center justify-center">-</button>
-          <div class="py-1 px-2 text-lg flex items-center justify- center">1</div>
-          <button class="cursor-pointer py-1 pl-2 pr-4 text-lg flex items- center justify-center">+</button>
+          <button class="cursor-pointer py-1 pl-4 pr-2 text-lg flex items- center justify-center" @click="decreaseQuantity">-</button>
+          <div class="py-1 px-2 text-lg flex items-center justify- center">{{ quantity }}</div>
+          <button class="cursor-pointer py-1 pl-2 pr-4 text-lg flex items- center justify-center" @click="increaseQuantity">+</button>
         </div>
         <div class="flex items-center gap-2 border border-gray-300 px-3 py-2 rounded-full cursor-pointer" @click="$emit('removeCart', props.product.id)">
             <i class="ri-delete-bin-7-fill text-sm text-gray-600"></i>
